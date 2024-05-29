b3f888fad0c422d0821c51f8009103b899a80568
tomlukewarden <tomlukeprogramming@outlook.com>
2024-05-29 14:32:18 +0100

    Dashboard Styles elevated

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -4,40 +4,52 @@ function Dashboard() {
   return (
     <>
       <section className="container">
-        <ul className="nav">
-          <li>
-        
-            <img src="assests/sent.png" alt="image of mail" width={37} />
-            Mail
-          </li>
-          <li>
-            <img src="assests/schedule.png" alt="Calender" width={37} />
-            Important Dates
-          </li>
-          <li>
-            <img src="assests/folder.png" alt="Folder" width={37} />
-            Case File
-          </li>
-          <li>
-            <img src="assests/card-payment.png" alt="Card Payment" width={37} />
-            Fees
-          </li>
-          <li>
-            <img src="assests/contact-mail.png" alt="Contact" width={37} />
-            Contact
-          </li>
-          <li>
-            <img src="assests/user.png" alt="Profile" width={37} />
-            My Profile
-          </li>
-        </ul>
-
-        <h1 className="title">Your Dashboard</h1>
-
-        <div className="profile-button">
-          <button className="sign-out">Sign Out</button>
-          <div className="profile-circle">PC</div>
+        <div className="nav-container">
+          <ul className="nav">
+            <li>
+              <img src="assests/nav-bar-icons/sent.png" alt="image of mail" width={37} />
+              Mail
+            </li>
+            <li>
+              <img src="assests/nav-bar-icons/schedule.png" alt="Calender" width={37} />
+              Important Dates
+            </li>
+            <li>
+              <img src="assests/nav-bar-icons/folder.png" alt="Folder" width={37} />
+              Case File
+            </li>
+            <li>
+              <img src="assests/nav-bar-icons/card-payment.png" alt="Card Payment" width={37} />
+              Fees
+            </li>
+            <li>
+              <img src="assests/nav-bar-icons/contact-mail.png" alt="Contact" width={37} />
+              Contact
+            </li>
+            <li>
+              <img src="assests/nav-bar-icons/user.png" alt="Profile" width={37} />
+              My Profile
+            </li>
+            <li><button className="feedback">Give Us Feedback</button></li>
+          </ul>
         </div>
+        <main className="main-container">
+          <h1 className="title">Your Dashboard</h1>
+
+          <div className="profile-button">
+            <button className="sign-out">Sign Out</button>
+            <div className="profile-circle"></div>
+          </div>
+
+          <div className="banners-container">
+            <span className="date-banner">
+              Your Next Court Date is: {/*ADD JS HERE TO GENERATE THE MOST RECENT DATE FOR CLIENT*/}
+            </span>
+            <span className="fee-banner">
+              Your Outstanding Fees are:
+            </span>
+          </div>
+        </main>
       </section>
     </>
   );
