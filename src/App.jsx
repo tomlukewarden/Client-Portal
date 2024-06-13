09bcf18cb624af2941c95ae503d39103f69fff86
tomlukewarden <tomlukeprogramming@outlook.com>
2024-06-13 14:30:33 +0100

    Cleaning up contact form

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ImportantDates from '../src/client/Pages/Important-Dates/ImportantDates';
 import Mailbox from '../src/client/Pages/Mail/Mailbox';
 import Profile from '../src/client/Pages/Profile/Profile';
 import CaseFile from '../src/client/Pages/Case-File/CaseFile';
+import Contact from './client/Pages/Contact/Contact';
 import Fees from './client/Pages/Fees/Fees';
 import './App.css';
 
@@ -16,7 +17,8 @@ function App() {
         <Route path="/" element={<Dashboard/>} />
         <Route path="/feedback" element={<Feedback/>} />
         <Route path="/important-dates" element={<ImportantDates/>} />
-        <Route path='/fees' element={<Fees/>}></Route>
+        <Route path='/fees' element={<Fees/>}/>
+        <Route path='/contact' element={<Contact/>}/>
         <Route path="/mailbox" element={<Mailbox/>} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="/case-file" element={<CaseFile/>} />
