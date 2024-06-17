bf9633339e0cd20fb9e7cd54c84eb4d41650cb55
tom <141278466+tomlukewarden@users.noreply.github.com>
2024-06-17 16:34:51 +0100

    Added Routes

---
+++ backend/routes/user.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { signUpUser, loginUser } from '../controllers/users.js';
+
+const router = express.Router()
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await loginUser(email, password);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+});
+
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const newUser = await signUpUser(email, password);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
